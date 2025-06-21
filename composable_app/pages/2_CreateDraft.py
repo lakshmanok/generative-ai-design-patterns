e2bb87135527bfa1f9a858883f0169e67dc95843
V Lakshmanan <lak@vlakshman.com>
2025-06-21 13:02:05 -0700

    Add a chat interface to allow the user to modify the draft; log such changes to improve the model

--- composable_app/pages/2_CreateDraft.py
+++ composable_app/pages/2_CreateDraft.py
@@ -2,8 +2,12 @@ import streamlit as st
 import logging
 import asyncio
 from composable_app.agents.article import Article
+from composable_app.utils.human_feedback import record_human_feedback
 
 logger = logging.getLogger(__name__)
+
+def keywords_to_string(a: Article) -> str:
+    return '\n'.join(a.index_keywords)
 
 @st.cache_resource
 def write_about(writer_name, topic) -> Article:
@@ -21,11 +25,47 @@ def write_draft():
 
         topic = st.session_state.topic
         writer = st.session_state.writer
-        draft = write_about(writer.name(), topic)
-        draft_textbox = st.markdown(draft.to_markdown())
+        ai_generated_draft = write_about(writer.name(), topic)
+
+        if "ai_generated_draft" not in st.session_state or st.session_state.ai_generated_draft != ai_generated_draft:
+            # new topic perhaps; this draft was generated for the first time, and not pulled from cache
+            st.session_state.ai_generated_draft = ai_generated_draft
+            st.session_state.draft = ai_generated_draft
+
+        # display current version
+        draft_title = st.text_area(label="Title", value=st.session_state.draft.title)
+        draft_lesson = st.text_area(label="Lesson", value=st.session_state.draft.key_lesson)
+        draft_text = st.text_area(label="Text", value=st.session_state.draft.full_text, height=400)
+        draft_keywords = st.text_area(label="Keywords", value=keywords_to_string(st.session_state.draft), height=150)
+
+        # allow user to modify through a chat interface also
+        st.subheader("Modify draft")
+        def modify_draft():
+            modify_instruction = st.session_state.modify_instruction
+            logger.info(f"Updating draft to instructions: {modify_instruction}")
+            draft = asyncio.run(writer.revise_article(topic, st.session_state.draft, modify_instruction))
+            logger.info(draft.full_text)
+            st.session_state.draft = draft  # but keep the original as "ai_generated_draft"
+            # because this is a callback, it redraws the page
+
+        with st.form("Modification form", clear_on_submit=True):
+            st.text_input(label="Modification instructions", value="", key="modify_instruction")
+            st.form_submit_button(label="Modify", on_click=modify_draft)
+
 
         if st.button("Next"):
-            st.session_state.draft = draft
+            # grab from UI
+            st.session_state.draft.title = draft_title
+            st.session_state.draft.key_lesson = draft_lesson
+            st.session_state.draft.full_text = draft_text
+            st.session_state.draft.index_keywords = draft_keywords.split('\n')
+            # has it changed?
+            if st.session_state.draft != st.session_state.ai_generated_draft:
+                record_human_feedback("initial_draft",
+                                      ai_input=topic,
+                                      ai_response=st.session_state.ai_generated_draft,
+                                      human_choice=st.session_state.draft)
+                logger.info(f"User has draft to {st.session_state.draft}")
             st.switch_page("pages/3_PanelReview1.py")
 
     except Exception as e:
